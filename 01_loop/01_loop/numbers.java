dd13ead652116241762395bd8f89adb92743de47
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2024-12-22 23:09:54 +0330

    print number from 0 tp 9

--- 01_loop/01_loop/numbers.java
+++ 01_loop/01_loop/numbers.java
@@ -7,5 +7,14 @@
  */
 public class numbers
 {
-    
+    public static void LoopNumbers(String[] args)
+    {
+        int number = 0;
+        
+        /* print numbers in 0 to 9 with for loop */
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
 }
