3c4a0ffb91ec40859ebc686589eca5ac8977169b
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-01-05 22:54:55 +0330

    modification of cross Table

--- 02_crossTable/crossTable/src/Main.java
+++ 02_crossTable/crossTable/src/Main.java
@@ -5,5 +5,13 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("This is cross table Application!");
+
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                System.out.print(j*i + " ");
+            }
+            System.out.println();
+        }
     }
 }
